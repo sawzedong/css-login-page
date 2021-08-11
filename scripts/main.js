89a99d6ea13011683a38f081f41a5c397936ced1
sawzedong <saw.zedong@dhs.sg>
2021-08-11 16:00:38 +0800

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+function checkPassword(user, pwd) {
+    if (user === pwd) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+function submitForm() {
+    // e.preventDefault()
+    return false;
+}
+
+// go through?
